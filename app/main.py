7a0f01348fd056926910d44b7a8388b08b23865e
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-07-14 21:14:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ class CarWashStation:
                  count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
-        self. average_rating = round(average_rating, 1)
+        self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
     def calculate_washing_price(self, car: Car) -> float:
